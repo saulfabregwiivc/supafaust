051073cf6ffed4ccb638addba01762ee658b15d2
twinaphex <libretro@gmail.com>
2020-07-10 16:15:59 +0200

    Remove these

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git -C . archive --format=tar --prefix=supafaust/ HEAD^{tree} | xz > "supafaust.tar.xz" && \
-zip supafaust.tar.xz.zip supafaust.tar.xz && \
-./build-arm.sh && \
-./build-x86.sh && \
-true
